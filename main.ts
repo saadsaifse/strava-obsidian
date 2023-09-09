2d1eaef505fcfd03eb56abf5f566344e67d6a463
Saad Saif <msaadsaif5@gmail.com>
2023-09-09 15:42:16 -0400

    use system browser for auth instead

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
 import {
 	App,
-	Editor,
-	MarkdownView,
 	Modal,
 	Notice,
 	Plugin,
@@ -9,46 +7,72 @@ import {
 	Setting,
 	TFolder,
 } from 'obsidian'
-import { default as stravaApi, Strava } from 'strava-v3'
-//import request from 'request';
-import { BrowserWindow } from '@electron/remote'
-import * as fs from 'fs/promises'
+import { default as stravaApi, AuthenticationConfig } from 'strava-v3'
 import * as path from 'path'
 
-// Remember to rename these classes and interfaces!
-
 interface StravaAuthSettings {
-	token: any
+	stravaConfig: AuthenticationConfig
+	token: any // the response object from get token
 	accessCode: string
 }
 
 interface StravaActivitiesSettings {
-	mySetting: string
 	authSetting: StravaAuthSettings
 }
 
 const DEFAULT_SETTINGS: StravaActivitiesSettings = {
-	mySetting: 'default',
-	authSetting: { token: '', accessCode: '' },
+	authSetting: {
+		token: '',
+		accessCode: '',
+		stravaConfig: {
+			access_token: '',
+			client_id: '113274', // TODO: reset after input handler is implemented
+			client_secret: 'a596836c309eb7f08067aa7504907664998c896f', // TODO: reset after input handler is implemented
+			redirect_uri: 'obsidian://obsidianforstrava/callback',
+		},
+	},
 }
 
 export default class StravaActivities extends Plugin {
-	settings: StravaActivitiesSettings
+	settings = DEFAULT_SETTINGS
 
 	async onload() {
+		this.registerObsidianProtocolHandler(
+			'obsidianforstrava/callback',
+			async (args) => {
+				if (args.scope != 'read,activity:read_all') {
+					new Notice('Please authorize required permissions.')
+					return
+				}
+				try {
+					await this.getTokenFromAccessCode(args.code)
+					new Notice('Authenticated with Strava')
+				} catch (err) {
+					new Notice('Could not authenticate user')
+				}
+			}
+		)
+
 		await this.loadSettings()
 
-		// This creates an icon in the left ribbon.
+		this.addSettingTab(new StravaActivitiesSettingTab(this.app, this))
+
+		this.addCommand({
+			id: 'authenticate-command',
+			name: 'Authenticate with Strava',
+			callback: this.onAuthenticateCommand,
+		})
+
 		const ribbonIconEl = this.addRibbonIcon(
 			'dice',
 			'Strava Activities',
 			async (evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				if (this.settings.authSetting.token.access_token) {
+				if (this.settings.authSetting.stravaConfig.access_token != '') {
 					// TODO: check if refresh is needed
-					const activities = await stravaApi.athlete.listActivities(
-						{}
-					)
+					const activities = await stravaApi.athlete.listActivities({
+						page: 1,
+						per_page: 99,
+					})
 					console.log(activities)
 					try {
 						if (
@@ -59,7 +83,10 @@ export default class StravaActivities extends Plugin {
 						} else {
 							await this.app.vault.createFolder('Strava')
 						}
-						const filePath = path.join('Strava', 'Activities.json')
+						const filePath = path.join(
+							'Strava',
+							`Activities_SyncedAt_${Date.now()}.json`
+						)
 						const jsonData = JSON.stringify(activities, null, 2)
 						console.log(jsonData)
 						await this.app.vault.create(filePath, jsonData)
@@ -80,65 +107,12 @@ export default class StravaActivities extends Plugin {
 		)
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class')
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem()
-		statusBarItemEl.setText('Status Bar Text')
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open()
-			},
-		})
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection())
-				editor.replaceSelection('Sample Editor Command')
-			},
-		})
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView)
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open()
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true
-				}
-			},
-		})
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this))
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt)
-		})
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000)
-		)
 	}
 
-	onunload() {}
+	onunload() {
+		this.settings = DEFAULT_SETTINGS
+		this.saveSettings()
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign(
@@ -151,21 +125,22 @@ export default class StravaActivities extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings)
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app)
+	getTokenFromAccessCode = async (code: string) => {
+		const token = await stravaApi.oauth.getToken(code)
+		this.settings.authSetting.token = token
+		this.settings.authSetting.stravaConfig.access_token = token.access_token
+		this.saveSettings()
+		stravaApi.config(this.settings.authSetting.stravaConfig)
+		stravaApi.client(this.settings.authSetting.stravaConfig.access_token)
 	}
 
-	onOpen() {
-		const { contentEl } = this
-		contentEl.setText('Woah!')
-	}
-
-	onClose() {
-		const { contentEl } = this
-		contentEl.empty()
+	onAuthenticateCommand = async () => {
+		stravaApi.config(this.settings.authSetting.stravaConfig)
+		const url = await stravaApi.oauth.getRequestAccessURL({
+			scope: 'activity:read_all',
+		})
+		await open(url, undefined)
 	}
 }
 
@@ -183,6 +158,7 @@ class StravaApplicationDetailsModal extends Modal {
 		const clientIdElement = form.createEl('input', {
 			type: 'number',
 			attr: { id: 'clientId', name: 'clientId' },
+			value: this.plugin.settings.authSetting.stravaConfig.client_id,
 		})
 		form.createEl('br')
 		form.createEl('br')
@@ -190,94 +166,30 @@ class StravaApplicationDetailsModal extends Modal {
 		const clientSecretElement = form.createEl('input', {
 			type: 'password',
 			attr: { id: 'clientSecret', name: 'clientSecret' },
+			value: this.plugin.settings.authSetting.stravaConfig.client_secret,
 		})
 		form.createEl('br')
 		form.createEl('br')
-		const submitInputElement = form.createEl('input', {
+		const saveInputElement = form.createEl('input', {
 			type: 'button',
-			value: 'Authenticate',
+			value: 'Save',
 		})
-		submitInputElement.onClickEvent(async (el) => {
-			stravaApi.config({
-				access_token: '',
-				client_id: '113274',
-				client_secret: '',
-				redirect_uri: 'http://localhost/callback',
-			})
-			var accessCode = ''
-			const url = await stravaApi.oauth.getRequestAccessURL({
-				scope: 'activity:read_all',
-			})
-			console.log('browse to ' + url)
-			const window = new BrowserWindow({
-				width: 600,
-				height: 800,
-				webPreferences: {
-					nodeIntegration: false, // We recommend disabling nodeIntegration for security.
-					contextIsolation: true, // We recommend enabling contextIsolation for security.
-					// see https://github.com/electron/electron/blob/master/docs/tutorial/security.md
-				},
-			})
-
-			const {
-				session: { webRequest },
-			} = window.webContents
-
-			const filter = {
-				urls: ['http://localhost/callback*'],
-			}
-
-			webRequest.onBeforeRequest(filter, async ({ url }) => {
-				console.log(
-					'user granted previleges to temp credentials (requestToken) ' +
-						url
-				)
-				var redirectUrl = new URL(url)
-				accessCode = redirectUrl.searchParams.get('code') ?? ''
-				this.plugin.settings.authSetting.accessCode = accessCode
-				console.log('got access code', accessCode)
-				window.close()
-			})
-
-			window.loadURL(url)
-
-			window.on('closed', async () => {
-				console.log('window closed')
-
-				if (accessCode == '') {
-					console.log('failed to authorize user')
-				} else {
-					console.log('in else with access code', accessCode)
-					stravaApi.config({
-						access_token: '',
-						client_id: '113274',
-						client_secret:
-							'a596836c309eb7f08067aa7504907664998c896f',
-						redirect_uri: 'http://localhost/callback',
-					})
-					const token = await stravaApi.oauth.getToken(accessCode)
-					stravaApi.config({
-						access_token: token.access_token,
-						client_id: '113274',
-						client_secret:
-							'a596836c309eb7f08067aa7504907664998c896f',
-						redirect_uri: 'http://localhost/callback',
-					})
-					stravaApi.client(token.access_token)
-					this.plugin.settings.authSetting.token = token
-					console.log('token', token)
-				}
-			})
+		saveInputElement.onClickEvent(() => {
+			this.plugin.settings.authSetting.stravaConfig.client_id =
+				clientIdElement.value
+			this.plugin.settings.authSetting.stravaConfig.client_secret =
+				clientSecretElement.value
+			this.plugin.saveSettings()
+			this.close()
 		})
 	}
 
 	onClose() {
-		const { contentEl } = this
-		contentEl.empty()
+		this.contentEl.empty()
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
+class StravaActivitiesSettingTab extends PluginSettingTab {
 	plugin: StravaActivities
 
 	constructor(app: App, plugin: StravaActivities) {
@@ -287,25 +199,14 @@ class SampleSettingTab extends PluginSettingTab {
 
 	display(): void {
 		const { containerEl } = this
-
 		containerEl.empty()
 
-		// new Setting(containerEl)
-		// 	.setName('Setting #1')
-		// 	.setDesc('It\'s a secret')
-		// 	.addText(text => text
-		// 		.setPlaceholder('Enter your secret')
-		// 		.setValue(this.plugin.settings.mySetting)
-		// 		.onChange(async (value) => {
-		// 			this.plugin.settings.mySetting = value;
-		// 			await this.plugin.saveSettings();
-		// 		}));
 		new Setting(containerEl)
-			.setName('Connect Strava')
-			.setDesc('Connect to your Strava')
+			.setName('Enter Strava Credentials')
+			.setDesc('Set Strava Credentials')
 			.addButton((button) =>
 				button
-					.setButtonText('Enter Application Details')
+					.setButtonText('Enter Strava Credentials')
 					// TODO: set button class
 					.onClick((me) =>
 						new StravaApplicationDetailsModal(
@@ -313,6 +214,14 @@ class SampleSettingTab extends PluginSettingTab {
 							this.plugin
 						).open()
 					)
+			)
+		new Setting(containerEl)
+			.setName('Authenticate')
+			.setDesc('Authenticate your Strava account')
+			.addButton((button) =>
+				button
+					.setButtonText('Authenticate')
+					.onClick(this.plugin.onAuthenticateCommand)
 			)
 	}
 }
